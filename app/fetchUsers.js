9582b8662a9bb0c9061365dedd370b138b48e195
Doha <dohaamed22@gmail.com>
2024-11-28 04:42:47 +0200

    backend 1st commit

---
+++ app/fetchUsers.js
@@ -0,0 +1,6 @@
+"use server";
+import pool from "../lib/db";
+export default async function fecthUser(param) {
+  const resp = await pool.query("SELECT COUNT(*) from users");
+  return resp.rows;
+}
--- app/page.js
+++ app/page.js
@@ -1,6 +1,10 @@
-import LandingPage from "@/app/components/LandingPage";
+"use server";
+import LandingPage from "./components/LandingPage";
+import fecthUser from "./fetchUsers";
 
-export default function Home() {
+export default async function Home() {
+  const resp = await fecthUser();
+  console.log(resp);
   return (
       <>
           <LandingPage />
