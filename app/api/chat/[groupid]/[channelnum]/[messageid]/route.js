58a96f63fbbb248357d3524becaa1b037ac07e76
Doha <dohaamed22@gmail.com>
2024-11-28 20:57:32 +0200

    all chat endpoints

---
+++ app/api/chat/[groupid]/[channelnum]/[messageid]/route.js
@@ -0,0 +1,68 @@
+import pool from "@/lib/db";
+import { NextResponse } from "next/server";
+
+//get a message given its id
+export async function GET(request, { params }) {
+  try {
+    const par = await params;
+    console.log("Params:", par.messageid);
+    const messageinfo = await pool.query(
+      "SELECT * FROM Message WHERE Message_ID = $1",
+      [par.messageid]
+    );
+    if (messageinfo.rows.length === 0) {
+      return NextResponse.json({ error: "message not found" }, { status: 404 });
+    }
+    const messageattachments = await pool.query(
+      "SELECT * FROM messageAttachment WHERE Message_ID = $1",
+      [par.messageid]
+    );
+    const resp = {
+      ...messageinfo.rows[0],
+      attachments: messageattachments.rows,
+    };
+    return NextResponse.json(resp, { status: 200 });
+  } catch (err) {
+    console.error("Error fetching message:", err);
+    return NextResponse.json(
+      { error: "Failed to fetch message" },
+      { status: 500 }
+    );
+  }
+}
+
+// Update the content of a message
+export async function PATCH(request, { params }) {
+  try {
+    const par = await params;
+    const { content } = await request.json();
+    await pool.query("UPDATE Message SET Content = $1 WHERE Message_ID = $2", [
+      content,
+      par.messageid,
+    ]);
+    return NextResponse.json({ message: "Message updated" }, { status: 200 });
+  } catch (err) {
+    console.error("Error updating Message:", err);
+    return NextResponse.json(
+      { error: "Failed to update Message" },
+      { status: 500 }
+    );
+  }
+}
+
+//delete a specific message given its number and group id
+export async function DELETE(request, { params }) {
+  try {
+    const par = await params;
+    await pool.query("DELETE FROM Message WHERE Message_ID = $1", [
+      par.messageid,
+    ]);
+    return NextResponse.json({ message: "Message deleted" }, { status: 200 });
+  } catch (err) {
+    console.error("Error deleting Message: ", err);
+    return NextResponse.json(
+      { error: "Failed to delete Message" },
+      { status: 500 }
+    );
+  }
+}
