94092e1b9604a29844469fe05a184685ac7b20a8
Doha <dohaamed22@gmail.com>
2024-12-06 05:40:21 +0200

    some server components, {studentid}/grades

--- app/api/projects/route.js
+++ app/api/projects/route.js
@@ -7,23 +7,23 @@ export async function GET(request) {
   return NextResponse.json(resp.rows, { status: 200 });
 }
 
-//create a new project
-export async function POST(request) {
-  try {
-    const { name, coursecode, endDate, description, maxSize } =
-      await request.json();
+// //create a new project
+// export async function POST(request) {
+//   try {
+//     const { name, coursecode, endDate, description, maxSize } =
+//       await request.json();
 
-    const query =
-      "INSERT INTO Project (Project_Name, Course_Code, End_Date, Description, Max_team_size) VALUES ($1, $2, $3, $4, $5)";
-    const values = [name, coursecode, endDate, description, maxSize];
-    await pool.query(query, values);
-    return NextResponse.json({ message: "Project created" }, { status: 201 });
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Failed to create Project" },
-      {
-        status: 500,
-      }
-    );
-  }
-}
+//     const query =
+//       "INSERT INTO Project (Project_Name, Course_Code, End_Date, Description, Max_team_size) VALUES ($1, $2, $3, $4, $5)";
+//     const values = [name, coursecode, endDate, description, maxSize];
+//     await pool.query(query, values);
+//     return NextResponse.json({ message: "Project created" }, { status: 201 });
+//   } catch (error) {
+//     return NextResponse.json(
+//       { error: "Failed to create Project" },
+//       {
+//         status: 500,
+//       }
+//     );
+//   }
+// }
