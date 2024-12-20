d194b40943c25dfe5325f4a3611834dfab4d2c9c
hxed <youssef.noser04@eng-st.cu.edu.eg>
2024-12-20 11:34:12 +0200

    restore layout.js

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,13 +1,16 @@
+import {SidebarProvider} from "@/components/ui/sidebar";
+import {SidebarLeft} from "@/components/sidebar-left";
+
 export default function RootLayout({children}) {
     return (
-        // <SidebarProvider>
-        //   <SidebarLeft className="w-[var(--sidebar-width)]" />
+        <SidebarProvider>
+            <SidebarLeft className="w-[var(--sidebar-width)]" />
 
-        <main className=" mx-auto px-4 lg:px-8 w-full">
-            {children}
-        </main>
+            <main className=" mx-auto px-4 lg:px-8 w-full">
+                {children}
+            </main>
 
-        // {/* <SidebarRight className="hidden md:block top-0 right-0 h-full w-[320px] bg-background z-50" /> */}
-        // </SidebarProvider>
+            {/* <SidebarRight className="hidden md:block top-0 right-0 h-full w-[320px] bg-background z-50" /> */}
+        </SidebarProvider>
     );
 }
